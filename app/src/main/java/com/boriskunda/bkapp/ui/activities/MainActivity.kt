df6087f91b9c5e49b690bfe2572485c1db08ff89
BorisKunda <bskunda1992@gmail.com>
2021-02-23 15:23:50 +0200

    world countries screen - part 2 - sorting list menu

--- app/src/main/java/com/boriskunda/bkapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/boriskunda/bkapp/ui/activities/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.boriskunda.bkapp.ui.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.FragmentManager
 import com.boriskunda.bkapp.R
 import com.boriskunda.bkapp.ui.fragments.BorderCountriesFragment
@@ -10,12 +13,13 @@ import com.boriskunda.bkapp.ui.fragments.WorldCountriesFragment
 import com.boriskunda.bkapp.utils.BkConstants
 import com.boriskunda.bkapp.viewmodel.BkSharedViewModel
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var worldCountriesFragment: WorldCountriesFragment//blue
     private lateinit var borderCountriesFragment: BorderCountriesFragment//orange
-    private lateinit var fragmentManager:FragmentManager
-    private val bkSharedViewModel:BkSharedViewModel by viewModels()
+    private lateinit var fragmentManager: FragmentManager
+    private val bkSharedViewModel: BkSharedViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,21 +31,46 @@ class MainActivity : AppCompatActivity() {
 
     private fun observeVM() {
         bkSharedViewModel.goToWorldCountriesFragmentSle.observe(this, {
-            fragmentManager.beginTransaction().replace(R.id.fr_container_ll, worldCountriesFragment).addToBackStack(BkConstants.MAIN_FRAGMENT_CONTAINER_BACKSTACK).commit()
+            fragmentManager.beginTransaction().replace(R.id.fr_container_ll, worldCountriesFragment)
+                .addToBackStack(
+                    BkConstants.MAIN_FRAGMENT_CONTAINER_BACKSTACK
+                ).commit()
         })
 
         bkSharedViewModel.goToBorderCountriesFragmentSle.observe(this, {
-            fragmentManager.beginTransaction().replace(R.id.fr_container_ll, worldCountriesFragment).commit()
+            fragmentManager.beginTransaction().replace(R.id.fr_container_ll, worldCountriesFragment)
+                .commit()
         })
     }
 
 
-    private fun initFragments(){
+    private fun initFragments() {
         fragmentManager = supportFragmentManager
         worldCountriesFragment = WorldCountriesFragment()
         borderCountriesFragment = BorderCountriesFragment()
     }
 
+    /**menu*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.name_menu_item -> {
+                BkConstants.debugPrint("name")
+            }
+            R.id.area_menu_item -> {
+                BkConstants.debugPrint("area")
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 
 }
